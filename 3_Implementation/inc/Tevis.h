593462de09e908809d70a5272950d13ab3925250
SantruptiP <82207640+SantruptiP@users.noreply.github.com>
2021-04-10 16:13:48 +0530

    Update and rename calculator_operations.h to Tevis.h

---
+++ 3_Implementation/inc/Tevis.h
@@ -0,0 +1,35 @@
+/** 
+* @file Tevis.h
+* Tevis signalling of trucks that enter and exit the warehouse
+*
+*/
+#ifndef __TEVIS_H__
+#define __TEVIS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+/**
+*  Adds truck records
+* @param[in] truckid 
+* @param[in] truckmodel
+* @param[in] truckRegNo
+* @param[in] carryingCargo
+* @param[in] drivenBy
+* @param[in] arrivalTime
+*/
+void addTrkRec(int *truckID,char *truckModel,int *truckRegNo,char *carryingCargo,char *drivenBy,int *arrivalTime);
+
+/**
+*  Displays truck records
+* @param[in] truckid 
+* @param[in] truckmodel
+* @param[in] truckRegNo
+* @param[in] carryingCargo
+* @param[in] drivenBy
+* @param[in] arrivalTime
+*/
+void viewData(int *truckID,char *truckModel,int *truckRegNo,char *carryingCargo,char *drivenBy,int *arrivalTime);
+
+#endif  /* #define __TEVIS_H__ */
